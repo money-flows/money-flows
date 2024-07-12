9820b6ff03ddef6e63e3d60207ca5f1ac3bd7a11
neoki <ot07neoki@gmail.com>
2024-07-13 00:09:56 +0900

    feat: add sign-up/sign-in pages

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,31 @@
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A47]">
+            おかえりなさい！
+          </h1>
+          <p className="text-base text-[#7E8CA0]">
+            収支の管理を続けるには、ログインしてください。
+          </p>
+        </div>
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
+        <Image src="/logo.svg" height={100} width={100} alt="" />
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,29 @@
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A47]">ようこそ！</h1>
+          <p className="text-base text-[#7E8CA0]">
+            収支の管理を始めるには、アカウントを作成してください。
+          </p>
+        </div>
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignUp />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
+        <Image src="/logo.svg" height={100} width={100} alt="" />
+      </div>
+    </div>
+  );
+}
