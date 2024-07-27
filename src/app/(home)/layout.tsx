cc0c5c9dae9fd44176f1647cfac1fcbb85527ace
neoki <ot07neoki@gmail.com>
2024-07-27 23:45:05 +0900

    fix: update header layout in home page

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -12,9 +12,9 @@ interface HomeLayoutProps {
 export default function HomeLayout({ children }: HomeLayoutProps) {
   return (
     <>
-      <header className="bg-gradient-to-b from-blue-700 to-blue-500 pb-32">
-        <div className="mx-auto max-w-screen-2xl p-4 lg:px-6">
-          <div className="mb-10 flex w-full items-center justify-between">
+      <header className="bg-gradient-to-b from-blue-700 to-blue-500 pb-48">
+        <div className="mx-auto max-w-screen-2xl p-4 pb-0 lg:px-6">
+          <div className="mb-12 flex w-full items-center justify-between">
             <div className="flex items-center lg:gap-x-8">
               <HeaderLogo />
               <Navigation />
@@ -30,7 +30,7 @@ export default function HomeLayout({ children }: HomeLayoutProps) {
         </div>
       </header>
       <main className="mx-auto w-full max-w-screen-2xl p-3 lg:p-6">
-        <div className="-mt-32">{children}</div>
+        <div className="-mt-48">{children}</div>
       </main>
     </>
   );
