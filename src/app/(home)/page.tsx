51598694a586724adf4a1c66ccaee778c8ba7c60
neoki <ot07neoki@gmail.com>
2024-07-27 21:07:26 +0900

    feat: add date range picker to dashboard page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,51 @@
 "use client";
 
+import { differenceInDays, format, startOfMonth } from "date-fns";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+
 import { DataCharts } from "@/components/data-charts";
 import { DataGrid } from "@/components/data-grid";
 import { TopCategoriesGrid } from "@/components/top-categories-grid";
+import { DateRange, DateRangePicker } from "@/components/ui/date-range-picker";
+
+const MAX_DATE_RANGE_DAYS = 365;
 
 export default function DashboardPage() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const from = searchParams.get("from") ?? startOfMonth(new Date());
+  const to = searchParams.get("to") ?? new Date();
+  console.log(from, to);
+
+  const handleDateRangeChange = ({ from, to }: DateRange) => {
+    if (!from || !to) {
+      return;
+    }
+
+    if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+      toast.error(
+        `日付の範囲は${MAX_DATE_RANGE_DAYS}日以内で入力してください。`,
+      );
+      return;
+    }
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("from", format(from, "yyyy-MM-dd"));
+    params.set("to", format(to, "yyyy-MM-dd"));
+    router.push(pathname + "?" + params.toString());
+  };
+
   return (
     <div className="mx-auto w-full max-w-screen-2xl">
+      <div className="mb-8">
+        <DateRangePicker
+          initialDateFrom={from}
+          initialDateTo={to}
+          onUpdate={handleDateRangeChange}
+        />
+      </div>
       <DataGrid />
       <TopCategoriesGrid />
       <DataCharts />
---
+++ src/components/ui/date-range-picker.tsx
@@ -0,0 +1,359 @@
+"use client";
+
+import { format } from "date-fns";
+import { CalendarIcon, Check } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
+
+import { cn } from "@/lib/utils";
+
+import { Button } from "./button";
+import { Calendar } from "./calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+import { Separator } from "./separator";
+
+export interface DateRangePickerProps {
+  onUpdate?: (range: DateRange) => void;
+  initialDateFrom?: Date | string;
+  initialDateTo?: Date | string;
+  locale?: string;
+}
+
+const formatDate = (date: Date) => {
+  return format(date, "yyyy/MM/dd");
+};
+
+const getDateAdjustedForTimezone = (dateInput: Date | string): Date => {
+  if (typeof dateInput === "string") {
+    // Split the date string to get year, month, and day parts
+    const parts = dateInput.split("-").map((part) => parseInt(part, 10));
+    // Create a new Date object using the local timezone
+    // Note: Month is 0-indexed, so subtract 1 from the month part
+    const date = new Date(parts[0], parts[1] - 1, parts[2]);
+    return date;
+  } else {
+    // If dateInput is already a Date object, return it directly
+    return dateInput;
+  }
+};
+
+export interface DateRange {
+  from: Date;
+  to: Date | undefined;
+}
+
+interface Preset {
+  name: string;
+  label: string;
+}
+
+// Define presets
+const PRESETS: Preset[] = [
+  { name: "thisWeek", label: "今週" },
+  { name: "lastWeek", label: "先週" },
+  { name: "thisMonth", label: "今月" },
+  { name: "lastMonth", label: "先月" },
+  { name: "last14", label: "過去14日間" },
+  { name: "last30", label: "過去30日間" },
+];
+
+/** The DateRangePicker component allows a user to select a range of dates */
+export const DateRangePicker = ({
+  initialDateFrom = new Date(new Date().setHours(0, 0, 0, 0)),
+  initialDateTo,
+  onUpdate,
+}: DateRangePickerProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [range, setRange] = useState<DateRange>({
+    from: getDateAdjustedForTimezone(initialDateFrom),
+    to: initialDateTo
+      ? getDateAdjustedForTimezone(initialDateTo)
+      : getDateAdjustedForTimezone(initialDateFrom),
+  });
+  const openedRangeRef = useRef<DateRange | undefined>();
+
+  const [selectedPreset, setSelectedPreset] = useState<string | undefined>(
+    undefined,
+  );
+
+  const [isSmallScreen, setIsSmallScreen] = useState(
+    typeof window !== "undefined" ? window.innerWidth < 960 : false,
+  );
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      setIsSmallScreen(window.innerWidth < 960);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const getPresetRange = (presetName: string): DateRange => {
+    const preset = PRESETS.find(({ name }) => name === presetName);
+    if (!preset) throw new Error(`Unknown date range preset: ${presetName}`);
+    const from = new Date();
+    const to = new Date();
+    const first = from.getDate() - from.getDay();
+
+    switch (preset.name) {
+      case "thisWeek":
+        from.setDate(first);
+        from.setHours(0, 0, 0, 0);
+        to.setHours(23, 59, 59, 999);
+        break;
+      case "lastWeek":
+        from.setDate(from.getDate() - 7 - from.getDay());
+        to.setDate(to.getDate() - to.getDay() - 1);
+        from.setHours(0, 0, 0, 0);
+        to.setHours(23, 59, 59, 999);
+        break;
+      case "thisMonth":
+        from.setDate(1);
+        from.setHours(0, 0, 0, 0);
+        to.setHours(23, 59, 59, 999);
+        break;
+      case "lastMonth":
+        from.setMonth(from.getMonth() - 1);
+        from.setDate(1);
+        from.setHours(0, 0, 0, 0);
+        to.setDate(0);
+        to.setHours(23, 59, 59, 999);
+        break;
+      case "last14":
+        from.setDate(from.getDate() - 13);
+        from.setHours(0, 0, 0, 0);
+        to.setHours(23, 59, 59, 999);
+        break;
+      case "last30":
+        from.setDate(from.getDate() - 29);
+        from.setHours(0, 0, 0, 0);
+        to.setHours(23, 59, 59, 999);
+        break;
+    }
+
+    return { from, to };
+  };
+
+  const setPreset = (preset: string): void => {
+    const range = getPresetRange(preset);
+    setRange(range);
+  };
+
+  const checkPreset = (): void => {
+    for (const preset of PRESETS) {
+      const presetRange = getPresetRange(preset.name);
+
+      const normalizedRangeFrom = new Date(range.from);
+      normalizedRangeFrom.setHours(0, 0, 0, 0);
+      const normalizedPresetFrom = new Date(
+        presetRange.from.setHours(0, 0, 0, 0),
+      );
+
+      const normalizedRangeTo = new Date(range.to ?? 0);
+      normalizedRangeTo.setHours(0, 0, 0, 0);
+      const normalizedPresetTo = new Date(
+        presetRange.to?.setHours(0, 0, 0, 0) ?? 0,
+      );
+
+      if (
+        normalizedRangeFrom.getTime() === normalizedPresetFrom.getTime() &&
+        normalizedRangeTo.getTime() === normalizedPresetTo.getTime()
+      ) {
+        setSelectedPreset(preset.name);
+        return;
+      }
+    }
+
+    setSelectedPreset(undefined);
+  };
+
+  const resetValues = (): void => {
+    setRange({
+      from:
+        typeof initialDateFrom === "string"
+          ? getDateAdjustedForTimezone(initialDateFrom)
+          : initialDateFrom,
+      to: initialDateTo
+        ? typeof initialDateTo === "string"
+          ? getDateAdjustedForTimezone(initialDateTo)
+          : initialDateTo
+        : typeof initialDateFrom === "string"
+          ? getDateAdjustedForTimezone(initialDateFrom)
+          : initialDateFrom,
+    });
+  };
+
+  useEffect(() => {
+    checkPreset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [range]);
+
+  const PresetButton = ({
+    preset,
+    label,
+    isSelected,
+  }: {
+    preset: string;
+    label: string;
+    isSelected: boolean;
+  }): JSX.Element => (
+    <Button
+      className={cn(
+        "w-full font-medium justify-start pl-1 pr-6",
+        isSelected && "pointer-events-none",
+      )}
+      size="sm"
+      variant="ghost"
+      onClick={() => {
+        setPreset(preset);
+      }}
+    >
+      <>
+        <span className={cn("pr-2 opacity-0", isSelected && "opacity-70")}>
+          <Check className="size-4" />
+        </span>
+        {label}
+      </>
+    </Button>
+  );
+
+  // Helper function to check if two date ranges are equal
+  const areRangesEqual = (a?: DateRange, b?: DateRange): boolean => {
+    if (!a || !b) return a === b; // If either is undefined, return true if both are undefined
+    return (
+      a.from.getTime() === b.from.getTime() &&
+      (!a.to || !b.to || a.to.getTime() === b.to.getTime())
+    );
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      openedRangeRef.current = range;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen]);
+
+  return (
+    <Popover
+      modal={true}
+      open={isOpen}
+      onOpenChange={(open: boolean) => {
+        if (!open) {
+          resetValues();
+        }
+        setIsOpen(open);
+      }}
+    >
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          className="w-full border-0 bg-white/10 font-normal text-white outline-none transition hover:bg-white/20 hover:text-white focus:bg-white/30 focus:ring-transparent focus:ring-offset-0 lg:w-auto"
+        >
+          <CalendarIcon className="mr-2 size-4" />
+          <span className="tabular-nums tracking-tighter">{`${formatDate(range.from)}${
+            range.to != null ? " - " + formatDate(range.to) : ""
+          }`}</span>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <div className="flex py-2">
+          <div className="flex">
+            <div className="flex flex-col">
+              {isSmallScreen && (
+                <Select
+                  defaultValue={selectedPreset}
+                  onValueChange={(value) => {
+                    setPreset(value);
+                  }}
+                >
+                  <div className="px-4 py-2">
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                  </div>
+                  <SelectContent>
+                    {PRESETS.map((preset) => (
+                      <SelectItem key={preset.name} value={preset.name}>
+                        {preset.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              )}
+              <div>
+                <Calendar
+                  mode="range"
+                  onSelect={(value: { from?: Date; to?: Date } | undefined) => {
+                    if (value?.from != null) {
+                      setRange({ from: value.from, to: value?.to });
+                    }
+                  }}
+                  selected={range}
+                  numberOfMonths={isSmallScreen ? 1 : 2}
+                  defaultMonth={
+                    new Date(
+                      new Date().setMonth(
+                        new Date().getMonth() - (isSmallScreen ? 0 : 1),
+                      ),
+                    )
+                  }
+                />
+              </div>
+            </div>
+          </div>
+          {!isSmallScreen && (
+            <>
+              <Separator orientation="vertical" className="h-auto" />
+              <div className="flex w-full flex-col items-end gap-1 p-2">
+                <div className="flex w-full flex-col items-end gap-1">
+                  {PRESETS.map((preset) => (
+                    <PresetButton
+                      key={preset.name}
+                      preset={preset.name}
+                      label={preset.label}
+                      isSelected={selectedPreset === preset.name}
+                    />
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+        <div className="flex justify-end gap-2 p-3 pt-0 lg:pt-1">
+          <Button
+            onClick={() => {
+              setIsOpen(false);
+              resetValues();
+            }}
+            variant="ghost"
+          >
+            やめる
+          </Button>
+          <Button
+            onClick={() => {
+              setIsOpen(false);
+              if (!areRangesEqual(range, openedRangeRef.current)) {
+                onUpdate?.(range);
+              }
+            }}
+          >
+            更新
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+DateRangePicker.displayName = "DateRangePicker";
