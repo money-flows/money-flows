cc5760b6b40c711b61fbe36f427b75001f2872e5
neoki <ot07neoki@gmail.com>
2024-07-31 08:53:53 +0900

    fix: update accounts page layout

--- src/app/(dashboard)/settings/accounts/page.tsx
+++ src/app/(dashboard)/settings/accounts/page.tsx
@@ -4,7 +4,7 @@ import { Plus } from "lucide-react";
 
 import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { H1 } from "@/components/ui/h1";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
@@ -18,34 +18,26 @@ export default function AccountsPage() {
 
   if (accountsQuery.isLoading) {
     return (
-      <Card>
-        <CardHeader>
-          <div className="flex flex-col gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-            <Skeleton className="h-9 w-20" />
-            <Skeleton className="h-9 w-full lg:w-[7.5rem]" />
-          </div>
-        </CardHeader>
-        <CardContent>
-          <Skeleton className="h-48 w-full" />
-        </CardContent>
-      </Card>
+      <div className="space-y-4">
+        <div className="flex flex-col gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <Skeleton className="h-9 w-16" />
+          <Skeleton className="h-9 w-full lg:w-[7.5rem]" />
+        </div>
+        <Skeleton className="h-48 w-full" />
+      </div>
     );
   }
 
   return (
-    <Card>
-      <CardHeader>
-        <div className="flex flex-col gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-          <CardTitle>口座</CardTitle>
-          <Button size="sm" onClick={onOpen}>
-            <Plus className="mr-2 size-4" />
-            口座を追加
-          </Button>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <DataTable columns={columns} data={accounts} />
-      </CardContent>
-    </Card>
+    <div className="space-y-4">
+      <div className="flex flex-col gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+        <H1>口座</H1>
+        <Button size="sm" onClick={onOpen}>
+          <Plus className="mr-2 size-4" />
+          口座を追加
+        </Button>
+      </div>
+      <DataTable columns={columns} data={accounts} />
+    </div>
   );
 }
