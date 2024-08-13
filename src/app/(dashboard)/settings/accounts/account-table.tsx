e8b7803c15a5c176559671624475e42405b7c329
neoki <ot07neoki@gmail.com>
2024-08-13 19:04:45 +0900

    fix: don't use suspense in fetching data in accounts page

--- src/app/(dashboard)/settings/accounts/account-table.tsx
+++ src/app/(dashboard)/settings/accounts/account-table.tsx
@@ -1,31 +1,21 @@
 "use client";
 
-import { Suspense } from "react";
-
 import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
-import { useGetAccountsSuspense } from "@/features/accounts/api/use-get-accounts";
-import { withClientSideAuthGuard } from "@/features/auth/hocs/with-client-side-auth-guard";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 import { columns } from "./columns";
 
-function AccountTableLoading() {
-  return <Skeleton className="h-48 w-full" />;
+export function AccountTable() {
+  const accountsQuery = useGetAccounts();
+
+  if (accountsQuery.isPending) {
+    return <Skeleton className="h-48 w-full" />;
+  }
+
+  if (accountsQuery.isError) {
+    return <p>エラーが発生しました</p>;
+  }
+
+  return <DataTable columns={columns} data={accountsQuery.data} />;
 }
-
-function AccountTableInner() {
-  const { data: accounts } = useGetAccountsSuspense();
-
-  return <DataTable columns={columns} data={accounts} />;
-}
-
-export const AccountTable = withClientSideAuthGuard(
-  function AccountTable() {
-    return (
-      <Suspense fallback={<AccountTableLoading />}>
-        <AccountTableInner />
-      </Suspense>
-    );
-  },
-  <AccountTableLoading />,
-);
--- src/app/(dashboard)/settings/accounts/page.tsx
+++ src/app/(dashboard)/settings/accounts/page.tsx
@@ -1,14 +1,24 @@
+"use client";
+
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import { H1 } from "@/components/ui/h1";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 import { AccountTable } from "./account-table";
-import { AddAccountButton } from "./add-account-button";
 
 export default function AccountsPage() {
+  const { onOpen } = useNewAccount();
+
   return (
     <div className="space-y-4">
       <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <H1>口座</H1>
-        <AddAccountButton />
+        <Button size="sm" onClick={onOpen}>
+          <Plus className="mr-2 size-4" />
+          口座を追加
+        </Button>
       </div>
       <AccountTable />
     </div>
