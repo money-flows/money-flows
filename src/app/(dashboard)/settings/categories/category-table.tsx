3e3a1a89b366840d028499098eb12d614a4e25a0
neoki <ot07neoki@gmail.com>
2024-08-13 19:07:42 +0900

    fix: don't use suspense in fetching data in categories page

--- src/app/(dashboard)/settings/categories/category-table.tsx
+++ src/app/(dashboard)/settings/categories/category-table.tsx
@@ -1,25 +1,26 @@
 "use client";
 
-import { Suspense } from "react";
-
 import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
-import { withClientSideAuthGuard } from "@/features/auth/hocs/with-client-side-auth-guard";
-import { useGetCategoriesSuspense } from "@/features/categories/api/use-get-categories";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
 
 import { columns } from "./columns";
 
-function CategoryTableLoading() {
-  return <Skeleton className="h-48 w-full" />;
+export function CategoryTable() {
+  const categoriesQuery = useGetCategories();
+
+  if (categoriesQuery.isPending) {
+    return <Skeleton className="h-48 w-full" />;
+  }
+
+  if (categoriesQuery.isError) {
+    return <p>エラーが発生しました</p>;
+  }
+
+  return <DataTable columns={columns} data={categoriesQuery.data} />;
 }
 
-function CategoryTableInner() {
-  const { data: accounts } = useGetCategoriesSuspense();
-
-  return <DataTable columns={columns} data={accounts} />;
-}
-
-export const CategoryTable = withClientSideAuthGuard(
+/* export const CategoryTable = withClientSideAuthGuard(
   function CategoryTable() {
     return (
       <Suspense fallback={<CategoryTableLoading />}>
@@ -28,4 +29,4 @@ export const CategoryTable = withClientSideAuthGuard(
     );
   },
   <CategoryTableLoading />,
-);
+); */
--- src/app/(dashboard)/settings/categories/page.tsx
+++ src/app/(dashboard)/settings/categories/page.tsx
@@ -1,14 +1,24 @@
-import { H1 } from "@/components/ui/h1";
+"use client";
 
-import { AddCategoryButton } from "./add-category-button";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { H1 } from "@/components/ui/h1";
+import { useNewCategory } from "@/features/categories/hooks/use-new-category";
+
 import { CategoryTable } from "./category-table";
 
 export default function CategoriesPage() {
+  const { onOpen } = useNewCategory();
+
   return (
     <div className="space-y-4">
       <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <H1>カテゴリー</H1>
-        <AddCategoryButton />
+        <Button size="sm" onClick={onOpen}>
+          <Plus className="mr-2 size-4" />
+          カテゴリーを追加
+        </Button>
       </div>
       <CategoryTable />
     </div>
