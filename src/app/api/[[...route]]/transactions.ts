65da6e7d9b56f72f8a6b41c224a24ea20c0d874e
neoki <ot07neoki@gmail.com>
2024-08-20 23:06:05 +0900

    fix: update logic for filling missing dates in transactions daily api

--- src/app/api/[[...route]]/transactions.ts
+++ src/app/api/[[...route]]/transactions.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
-import { endOfMonth } from "date-fns";
+import { endOfMonth, isAfter, isEqual, startOfMonth } from "date-fns";
 import { and, count, desc, eq, gt, inArray, lt, or, sql } from "drizzle-orm";
 import { Hono } from "hono";
 import groupBy from "lodash/groupBy";
@@ -322,7 +322,14 @@ export const transactions = new Hono()
       });
 
       const fillMissingDates = groupedByMonth.map(({ year, month, dates }) => {
-        const filledLastDate = endOfMonth(new Date(year, month - 1)).getDate();
+        const targetMonth = startOfMonth(new Date(year, month - 1));
+        const thisMonth = startOfMonth(new Date());
+
+        const filledLastDate = isAfter(targetMonth, thisMonth)
+          ? 0
+          : isEqual(targetMonth, thisMonth)
+            ? new Date().getDate()
+            : endOfMonth(targetMonth).getDate();
 
         const filledDates = Array.from({ length: filledLastDate }, (_, i) => {
           const dateData = dates.find((d) => d.date === i + 1);
