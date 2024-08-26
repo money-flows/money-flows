a328df50fdf23ba509561981322e654c1e692d4e
neoki <ot07neoki@gmail.com>
2024-08-27 00:04:47 +0900

    feat: add monthly income expense remaining chart

---
+++ src/app/(dashboard)/analytics/monthly-income-expense-remaining-chart.tsx
@@ -0,0 +1,223 @@
+"use client";
+
+import { useMemo, useState } from "react";
+import {
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  Line,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetTransactionsMonthly } from "@/features/transactions/api/use-get-transactions-monthly";
+
+const chartConfig = {
+  income: {
+    label: "収入",
+    color: `hsl(var(--chart-1))`,
+  },
+  expense: {
+    label: "支出",
+    color: `hsl(var(--chart-5))`,
+  },
+  remaining: {
+    label: "残高",
+    color: `hsl(var(--chart-3))`,
+  },
+};
+
+function yAxisTickFormatter(value: number) {
+  const formatter = new Intl.NumberFormat("ja-JP", {
+    notation: "compact",
+  });
+  return `${formatter.format(value)}円`;
+}
+
+interface MonthlyIncomeExpenseRemainingChartProps {
+  title: React.ReactNode;
+}
+
+export function MonthlyIncomeExpenseRemainingChart({
+  title,
+}: MonthlyIncomeExpenseRemainingChartProps) {
+  const [years] = useState([new Date().getFullYear()]);
+
+  const incomeQuery = useGetTransactionsMonthly({
+    types: ["income"],
+    years,
+  });
+
+  const expenseQuery = useGetTransactionsMonthly({
+    types: ["expense"],
+    years,
+  });
+
+  const remainingQuery = useGetTransactionsMonthly({
+    years,
+  });
+
+  const isPending =
+    incomeQuery.isPending || expenseQuery.isPending || remainingQuery.isPending;
+  const isError =
+    incomeQuery.isError || expenseQuery.isError || remainingQuery.isError;
+
+  const chartData = useMemo(() => {
+    if (isPending || isError) {
+      return [];
+    }
+
+    const groupByMonthData = incomeQuery.data.data.reduce(
+      (acc, { month, totalAmount }) => {
+        return {
+          ...acc,
+          [month]: {
+            ...acc[month],
+            income: totalAmount,
+          },
+        };
+      },
+      {
+        1: { month: 1 },
+        2: { month: 2 },
+        3: { month: 3 },
+        4: { month: 4 },
+        5: { month: 5 },
+        6: { month: 6 },
+        7: { month: 7 },
+        8: { month: 8 },
+        9: { month: 9 },
+        10: { month: 10 },
+        11: { month: 11 },
+        12: { month: 12 },
+      } as Record<number, Record<number, number>>,
+    );
+
+    const groupByMonthData2 = expenseQuery.data.data.reduce(
+      (acc, { month, totalAmount }) => {
+        return {
+          ...acc,
+          [month]: {
+            ...acc[month],
+            expense: totalAmount,
+          },
+        };
+      },
+      groupByMonthData,
+    );
+
+    const groupByMonthData3 = remainingQuery.data.data.reduce(
+      (acc, { month, totalAmount }) => {
+        return {
+          ...acc,
+          [month]: {
+            ...acc[month],
+            remaining: totalAmount,
+          },
+        };
+      },
+      groupByMonthData2,
+    );
+
+    return Object.values(groupByMonthData3);
+  }, [
+    incomeQuery.data,
+    expenseQuery.data,
+    remainingQuery.data,
+    isPending,
+    isError,
+  ]);
+
+  if (isPending) {
+    return (
+      <Card>
+        <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+          <CardTitle>{title}</CardTitle>
+        </CardHeader>
+        <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+          <Skeleton className="h-[350px] w-full" />
+        </CardContent>
+      </Card>
+    );
+  }
+
+  if (isError) {
+    return <p>エラーが発生しました</p>;
+  }
+
+  return (
+    <Card>
+      <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[350px] w-full"
+        >
+          <ComposedChart
+            accessibilityLayer
+            data={chartData}
+            margin={{ left: 12, right: 12, top: 12, bottom: 12 }}
+            barGap="-78.5%"
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => `${value}月`}
+            />
+            <YAxis
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={yAxisTickFormatter}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent
+                  labelFormatter={(_, [payload]) => {
+                    return `${payload?.payload?.month}月`;
+                  }}
+                  indicator="dot"
+                />
+              }
+            />
+            <Bar
+              dataKey="income"
+              fill="var(--color-income)"
+              fillOpacity={0.85}
+              radius={[4, 4, 0, 0]}
+            />
+            <Bar
+              dataKey="expense"
+              fill="var(--color-expense)"
+              fillOpacity={0.85}
+              radius={[4, 4, 0, 0]}
+            />
+            <Line
+              dataKey="remaining"
+              stroke="var(--color-remaining)"
+              strokeWidth={3}
+              dot={{ r: 4, strokeWidth: 1 }}
+            />
+            <ChartLegend content={<ChartLegendContent />} />
+          </ComposedChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/(dashboard)/analytics/page.tsx
+++ src/app/(dashboard)/analytics/page.tsx
@@ -4,12 +4,14 @@ import { H1 } from "@/components/ui/h1";
 
 import { ByCategoryBarChart } from "./by-category-bar-chart";
 import { DailyLineChart } from "./daily-line-chart";
+import { MonthlyIncomeExpenseRemainingChart } from "./monthly-income-expense-remaining-chart";
 import { MonthlyLineChart } from "./monthly-line-chart";
 
 export default function AnalyticsPage() {
   return (
     <div className="space-y-4">
       <H1>分析</H1>
+      <MonthlyIncomeExpenseRemainingChart title="収支の推移（年間）" />
       <MonthlyLineChart title="残高の推移（年間）" type="remaining" />
       <MonthlyLineChart title="収入の推移（年間）" type="income" />
       <MonthlyLineChart
