1c347c98551bb431f21249ee64a11fcdd16e782f
neoki <ot07neoki@gmail.com>
2024-10-30 20:49:30 +0900

    feat: add title in import-csv page

--- src/app/(dashboard)/transactions/import-csv/page.tsx
+++ src/app/(dashboard)/transactions/import-csv/page.tsx
@@ -3,22 +3,28 @@
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { H1 } from "@/components/ui/h1";
 
 import { Separator } from "./separator";
 
 export default function ImportCsvPage() {
   return (
-    <div className="flex w-full flex-col gap-6">
-      <Button asChild>
-        <Link href="/transactions/import-csv/manual">
-          手動で設定して取り込む
-        </Link>
-      </Button>
-      <Separator>カンタン取り込み</Separator>
-      <div className="flex flex-col gap-4">
-        <Button asChild variant="outline">
-          <Link href="/transactions/import-csv/zaim">ZaimのCSVを取り込む</Link>
+    <div className="space-y-4">
+      <H1>CSV取り込み</H1>
+      <div className="flex w-full flex-col gap-6">
+        <Button asChild>
+          <Link href="/transactions/import-csv/manual">
+            手動で設定して取り込む
+          </Link>
         </Button>
+        <Separator>カンタン取り込み</Separator>
+        <div className="flex flex-col gap-4">
+          <Button asChild variant="outline">
+            <Link href="/transactions/import-csv/zaim">
+              ZaimのCSVを取り込む
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
